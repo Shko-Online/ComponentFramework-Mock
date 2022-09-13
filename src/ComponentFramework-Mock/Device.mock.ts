5ca80813ad99ca57964ab80a2d6c529617a74402
asllanmakaj7 <107688754+asllanmakaj7@users.noreply.github.com>
2022-09-13 12:40:58 +0200

    ImageUploadControl tests

--- src/ComponentFramework-Mock/Device.mock.ts
+++ src/ComponentFramework-Mock/Device.mock.ts
@@ -16,38 +16,70 @@
 import { stub, SinonStub } from "sinon";
 
 export class DeviceMock implements ComponentFramework.Device {
-    captureAudio: SinonStub<[], Promise<ComponentFramework.FileObject>>;
-    captureImage: SinonStub<[options?: ComponentFramework.DeviceApi.CaptureImageOptions], Promise<ComponentFramework.FileObject>>;
-    captureVideo: SinonStub<[], Promise<ComponentFramework.FileObject>>;
-    getBarcodeValue: SinonStub<[], Promise<string>>;
-    getCurrentPosition: SinonStub<[], Promise<ComponentFramework.DeviceApi.Position>>;
-    pickFile: SinonStub<[options?: ComponentFramework.DeviceApi.PickFileOptions], Promise<ComponentFramework.FileObject[]>>;
-    constructor() {
-        this.captureAudio = stub();
+  captureAudio: SinonStub<[], Promise<ComponentFramework.FileObject>>;
+  captureImage: SinonStub<
+    [options?: ComponentFramework.DeviceApi.CaptureImageOptions],
+    Promise<ComponentFramework.FileObject>
+  >;
+  captureVideo: SinonStub<[], Promise<ComponentFramework.FileObject>>;
+  getBarcodeValue: SinonStub<[], Promise<string>>;
+  getCurrentPosition: SinonStub<
+    [],
+    Promise<ComponentFramework.DeviceApi.Position>
+  >;
+  pickFile: SinonStub<
+    [options?: ComponentFramework.DeviceApi.PickFileOptions],
+    Promise<ComponentFramework.FileObject[]>
+  >;
+  constructor() {
+    this.captureAudio = stub();
 
-        this.captureImage = stub();
+    this.captureImage = stub();
 
-        this.captureVideo = stub();
+    this.captureVideo = stub();
 
-        this.getBarcodeValue = stub();
+    this.getBarcodeValue = stub();
 
-        this.getCurrentPosition = stub();
-        const currentPositionPromise = new Promise<ComponentFramework.DeviceApi.Position>((resolve) => {
-            resolve({
-                coords: {
-                    accuracy: 141,
-                    latitude: 41.3415145,
-                    longitude: 19.7769355,
-                    altitude: 0,
-                    altitudeAccuracy: 0,
-                    heading: 0,
-                    speed: 0
-                },
-                timestamp: new Date()
-            })
+    this.getCurrentPosition = stub();
+    const currentPositionPromise =
+      new Promise<ComponentFramework.DeviceApi.Position>((resolve) => {
+        resolve({
+          coords: {
+            accuracy: 141,
+            latitude: 41.3415145,
+            longitude: 19.7769355,
+            altitude: 0,
+            altitudeAccuracy: 0,
+            heading: 0,
+            speed: 0,
+          },
+          timestamp: new Date(),
         });
-        this.getCurrentPosition.returns(currentPositionPromise);
-        this.pickFile = stub();
-
-    }
+      });
+    this.getCurrentPosition.returns(currentPositionPromise);
+    this.pickFile = stub();
+    const pickFilePromise =
+      new Promise<ComponentFramework.DeviceApi.PickFileOptions>((resolve) => {
+        resolve({
+          accept: " ",
+          allowMultipleFiles: false,
+          maximumAllowedFileSize: 1,
+        });
+      });
+    this.pickFile.callsFake(
+      (options?: ComponentFramework.DeviceApi.PickFileOptions) => {
+        return new Promise<ComponentFramework.FileObject[]>((resolve) => {
+          const configuredOptions = options || { accept: "image" };
+          resolve([
+            {
+              fileContent: "demo",
+              fileName: "fakeFile.png",
+              fileSize: 200,
+              mimeType: "image/png",
+            },
+          ]);
+        });
+      }
+    );
+  }
 }
