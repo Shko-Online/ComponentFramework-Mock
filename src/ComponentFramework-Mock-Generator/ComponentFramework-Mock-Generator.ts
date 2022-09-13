bad513a96c27b8868a52a8117616a594cbdb64c5
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-09-13 08:59:24 +0200

    Added SetControlResource

--- src/ComponentFramework-Mock-Generator/ComponentFramework-Mock-Generator.ts
+++ src/ComponentFramework-Mock-Generator/ComponentFramework-Mock-Generator.ts
@@ -1,16 +1,16 @@
 /*
-	Unless explicitly acquired and licensed from Licensor under another
-	license, the contents of this file are subject to the Reciprocal Public
-	License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
-	and You may not copy or use this file in either source code or executable
-	form, except in compliance with the terms and conditions of the RPL.
+    Unless explicitly acquired and licensed from Licensor under another
+    license, the contents of this file are subject to the Reciprocal Public
+    License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+    and You may not copy or use this file in either source code or executable
+    form, except in compliance with the terms and conditions of the RPL.
 
-	All software distributed under the RPL is provided strictly on an "AS
-	IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
-	LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
-	LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-	PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
-	language governing rights and limitations under the RPL. 
+    All software distributed under the RPL is provided strictly on an "AS
+    IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+    LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+    LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+    PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+    language governing rights and limitations under the RPL. 
 */
 
 import { spy, fake, SinonSpy, SinonSpiedInstance } from "sinon";
@@ -49,6 +49,18 @@ export class ComponentFrameworkMockGenerator<
         });
 
         this.container = container ?? document.createElement("div");
+    }
+
+    SetControlResource(resource: string) {
+        const xmlResource = new DOMParser().parseFromString(resource, 'text/xml');
+        const elements = xmlResource.getElementsByTagNameNS('', 'data');
+        this.context.resources.getString.callsFake((id) => {
+            for (let i = 0; i < elements.length; i++) {
+                if (elements[i].getAttribute('name') === id) {
+                    return elements[i].getElementsByTagName("value")[0].innerHTML;
+                }
+            }
+        })
     }
 
     ExecuteInit() {
