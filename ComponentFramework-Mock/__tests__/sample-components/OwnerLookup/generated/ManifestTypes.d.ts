76fc3397d0f6d1c126a04aaaefbd3a48f07ce8a4
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-12-28 07:51:18 +0100

    fix: Fixed a problem with Lookup values on update
    When a lookup property was changed the value saved on the database was incorrectly the array value

---
+++ ComponentFramework-Mock/__tests__/sample-components/OwnerLookup/generated/ManifestTypes.d.ts
@@ -0,0 +1,11 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    value: ComponentFramework.PropertyTypes.LookupProperty;
+}
+export interface IOutputs {
+    value?: ComponentFramework.LookupValue[];
+}
--- ComponentFramework-Mock/src/ComponentFramework-Mock-Generator/mockNotifyOutputChanged.ts
+++ ComponentFramework-Mock/src/ComponentFramework-Mock-Generator/mockNotifyOutputChanged.ts
@@ -4,8 +4,8 @@
 */
 
 import { SinonSpy, stub } from 'sinon';
-import { MultiSelectOptionSetPropertyMock } from '../ComponentFramework-Mock/PropertyTypes';
-import { arrayEqual } from '../utils';
+import { LookupPropertyMock, MultiSelectOptionSetPropertyMock } from '../ComponentFramework-Mock/PropertyTypes';
+import { arrayEqual, itemEqual } from '../utils';
 import { MockGenerator } from './MockGenerator';
 
 export const mockNotifyOutputChanged = <
@@ -19,20 +19,26 @@ export const mockNotifyOutputChanged = <
     mockGenerator.notifyOutputChanged = stub();
     mockGenerator.notifyOutputChanged.callsFake(() => {
         const updates = getOutputs?.();
+        if (!updates) return;
+
         mockGenerator.context.updatedProperties = [];
         for (const k in updates) {
             if (k in mockGenerator.context._parameters) {
+                let isLookup = false;
                 const property = mockGenerator.context._parameters[k];
 
                 if (Array.isArray(updates[k])) {
                     const arrayUpdate = updates[k] as number[];
+                    if (arrayUpdate && typeof arrayUpdate[0] === 'object') {
+                        isLookup = true;
+                    }
                     const property = mockGenerator.context._parameters[k] as MultiSelectOptionSetPropertyMock;
                     if (!arrayEqual(arrayUpdate, property.raw)) {
                         mockGenerator.context.updatedProperties.push(k);
                     }
-                } else if (typeof updates[k] === 'object') {
+                } /*else if (typeof updates[k] === 'object') {
                     // ToDo
-                } else {
+                }*/ else {
                     if (
                         (mockGenerator.context._parameters[k] as unknown as MultiSelectOptionSetPropertyMock).raw !==
                         updates[k]
@@ -40,6 +46,17 @@ export const mockNotifyOutputChanged = <
                         mockGenerator.context.updatedProperties.push(k);
                     }
                 }
+
+                if (isLookup) {
+                    mockGenerator.metadata.UpdateValue(
+                        (updates[k] as ComponentFramework.LookupValue[])[0],
+                        property._boundTable,
+                        property._boundColumn,
+                        property._boundRow,
+                    );
+                    continue;
+                }
+
                 mockGenerator.metadata.UpdateValue(
                     updates[k],
                     property._boundTable,
