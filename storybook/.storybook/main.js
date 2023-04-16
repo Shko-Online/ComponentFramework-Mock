415bc8f25dd5885bfd692265272b42f1007d2370
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2023-04-16 11:03:07 +0200

    docs: updated storybook

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -29,7 +29,7 @@ const config = {
       expanded: false // optional, true by default
     },
   },
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-webpack5",
