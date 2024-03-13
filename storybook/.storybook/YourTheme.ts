d411479000c7ccb18875dc2f015c9ebcaf22ac6b
asllanmakaj7 <animakaj7@gmail.com>
2024-03-13 09:03:20 +0100

    chore: fixed theme

---
+++ storybook/.storybook/YourTheme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: "Shko Online's Component Framework Mock",
+    brandUrl: 'https://github.com/shko-online/ComponentFramework-Mock',
+    brandImage: './Shko Online 92x32.svg',
+    brandTarget: '_self',
+    
+});
--- storybook/.storybook/manager.ts
+++ storybook/.storybook/manager.ts
@@ -1,13 +1,8 @@
-// import { AddonStore, addons } from '@storybook/manager';
+import { addons } from '@storybook/manager-api';
+import yourTheme from './YourTheme';
 
-// type Addon_Config<T extends AddonStore['setConfig']> = T extends (value: infer K) => void ? K : never;
-// type ThemeVars = Addon_Config<AddonStore['setConfig']>['theme'];
-
-// addons.setConfig({
-//     theme: {
-//         brandTitle: "Shko Online's Component Framework Mock",
-//         brandUrl: 'https://github.com/shko-online/ComponentFramework-Mock',
-//         brandImage: './Shko Online 92x32.svg',
-//     } as ThemeVars,
-//     showRoots: false,
-// });
+addons.setConfig({
+  theme: yourTheme,
+  sidebar:{showRoots:false,},
+ 
+});
